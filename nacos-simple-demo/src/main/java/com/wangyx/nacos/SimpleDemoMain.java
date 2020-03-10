06932a8074204a43118e8874274c8edda3b176db
wangyongxin <wangyongxin546@163.com>
2020-03-10 14:00:12 +0800

    添加用户 简单获取nacos配置

---
+++ nacos-simple-demo/src/main/java/com/wangyx/nacos/SimpleDemoMain.java
@@ -0,0 +1,52 @@
+package com.wangyx.nacos;
+
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.exception.NacosException;
+
+import java.util.Properties;
+
+public class SimpleDemoMain {
+
+    public static void main(String[] args) throws NacosException {
+//        test1();
+        test2();
+
+    }
+
+    private static void test1() throws NacosException {
+        //nacos 地址
+        String serverAddr = "127.0.0.1:8848";
+        //String dataId = "nacos-simple-demo.yaml";
+        String dataId = "nacos-simple-demo.yaml";
+        //Group
+        String group = "DEFAULT_GROUP";
+        Properties properties = new Properties();
+        properties.put("serverAddr", serverAddr);
+
+        ConfigService configService = NacosFactory.createConfigService(properties);
+        //获取配置 String dataId, String group, long timeoutMs
+        String content = configService.getConfig(dataId, group, 5000);
+        System.out.println(content);
+    }
+    //带命名空间 及 用户名密码
+    private static void test2() throws NacosException {
+        //nacos 地址
+        String serverAddr = "127.0.0.1:8848";
+        //String dataId = "nacos-simple-demo.yaml";
+        String dataId = "nacos-simple-demo.yaml";
+        String namespace = "d91431c4-c2bd-4186-b029-1f5817c4d764";//dev
+        //Group
+        String group = "DEFAULT_GROUP";
+        Properties properties = new Properties();
+        properties.put("serverAddr", serverAddr);
+        properties.put("namespace", namespace);
+        properties.put("username", "nacos");
+        properties.put("password", "nacos");
+
+        ConfigService configService = NacosFactory.createConfigService(properties);
+        //获取配置 String dataId, String group, long timeoutMs
+        String content = configService.getConfig(dataId, group, 5000);
+        System.out.println(content);
+    }
+}
--- password-encoder/src/test/java/PasswordEncoderUtil.java
+++ password-encoder/src/test/java/PasswordEncoderUtil.java
@@ -3,6 +3,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class PasswordEncoderUtil {
     public static void main(String[] args) {
         //生成nacos密码 直接覆盖数据库
-        System.out.println(new BCryptPasswordEncoder().encode("123"));
+        System.out.println(new BCryptPasswordEncoder().encode("123456"));
+        //insert into users(username, password, enabled) values ('test','$2a$10$S44FT9z7GtJmYK.BC65mouN6VAaOqFDc9g0uMDH6F/EcUalP9HB1K',1);
+        //insert roles (username, role) values ('test', 'ROLE_ADMIN');
     }
 }
